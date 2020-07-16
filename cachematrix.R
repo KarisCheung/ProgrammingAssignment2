27839055870c518c49af08164dd38f46ea89d34c
KarisCheung <67901412+KarisCheung@users.noreply.github.com>
2020-07-16 12:32:12 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Find inverse of a matrix if it has not been done previously
 
-## Write a short comment describing this function
-
+## Function makeCacheMatrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  #Set Function: Set x=y and i=null
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  #Get Function: To get x
+  get <- function() x
+  #Set Inverse Function: To set i = inverse (where function requires inverse to be given)
+  setinverse <- function(inverse) i <<- inverse
+  #Get Inverse Function: Print i (set as = inverse in setinverse)
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## Check if it has already been done, then return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #Check if already done
+  i <- x$getinverse()
+  if(!is.null(i)){
+    return(i)
+  }
+  #Get the matrix
+  matrix <- x$get()
+  #Solve for inverse
+  i <- solve(matrix, ...)
+  #Set value of inverse
+  x$setinverse(i)
+  i   
 }
+
+##Test
+test <- makeCacheMatrix(matrix(1:4,2,2))
+test
+test$get()
+cacheSolve(test)
+cacheSolve(test)
+test$getinverse()
